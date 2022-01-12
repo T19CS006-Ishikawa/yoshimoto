53f860dbd5be8f18eedd6ce4672ca99c8b0966e7
Shinichiro Watai <g19tk022@yamanashi.ac.jp>
2022-01-12 16:07:37 +0900

    メッセージ受信

---
+++ index.txt
@@ -0,0 +1,8 @@
+<?php
+
+require_once__DIR__ , '/vendor/autoload.php';
+
+$inputString = file_get_contents('php://input');
+error_log(&inputString);
+
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '52fe15554ecbd54f82ca3165cbab4b5823bd0144',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '52fe15554ecbd54f82ca3165cbab4b5823bd0144',
+            'dev_requirement' => false,
+        ),
+        'linecorp/line-bot-sdk' => array(
+            'pretty_version' => '3.6.0',
+            'version' => '3.6.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../linecorp/line-bot-sdk',
+            'aliases' => array(),
+            'reference' => 'e1515090eaf2106efdbdf51b1a6adf812844f54f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
