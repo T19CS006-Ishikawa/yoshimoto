ca72271cc58ccd2c5af344505eff3a3d81cb5e96
Shinichiro Watai <g19tk022@yamanashi.ac.jp>
2022-01-13 17:20:38 +0900

    メッセージ受信

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 //Composerでインストールしたライブラリの一括読み込み
 require_once __DIR__ . '/vendor/autoload.php';
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN')]);
 $bot = new \LINE\LINEBot($httpClient,['channelSecret => getenv('CHANNEL_SECRET')]);
 $signature = $_SERVER['HTTP_' , \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 $event = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
