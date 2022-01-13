fb0026b2f24fda5e17b06a0f4b784d0804d6b47e
Shinichiro Watai <g19tk022@yamanashi.ac.jp>
2022-01-13 17:26:05 +0900

    メッセージ返信

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
 $bot = new \LINE\LINEBot($httpClient,['channelSecret' => getenv('CHANNEL_SECRET')]);
 $signature = $_SERVER['HTTP_' , \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-$event = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
 
 foreach($events as $event){
 　//テキストを送信
