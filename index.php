a4c9c262a9606cd2f06fae9c7aa978bd37b630b1
Shinichiro Watai <g19tk022@yamanashi.ac.jp>
2022-01-13 17:19:33 +0900

    メッセージ受信

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
-$bot = new \LINE\LINEBot($httpClient,['channelSecret => getenv('ChannelSecret' => getenv('CHANNEL_SECRET')]);
+$bot = new \LINE\LINEBot($httpClient,['channelSecret => getenv('CHANNEL_SECRET')]);
 $signature = $_SERVER['HTTP_' , \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 $event = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
 
