e7710dd654f6f4741402d808730366ebcc1dd449
Shinichiro Watai <g19tk022@yamanashi.ac.jp>
2022-01-15 16:14:40 +0900

    local

--- index.php
+++ index.php
@@ -12,7 +12,9 @@ foreach ($events as $event){
 	//テキストを送信
 //	$bot->replyText($event->getReplyToken(), 'TextMessage');
 //	replyTextMessage($bot, $event->getReplyToken(),'TextMessage');
-	replyImageMessage($bot, $event->getReplyToken(),'https://' . $_SERVER['HTTP_HOST'] . '/imgs/original.jpg','https://' . $_SERVER['HTTP_HOST'] . '/imgs/preview.jpg');
+//	replyImageMessage($bot, $event->getReplyToken(),'https://' . $_SERVER['HTTP_HOST'] . '/imgs/original.jpg','https://' . $_SERVER['HTTP_HOST'] . '/imgs/preview.jpg');
+	//位置情報を返信
+	replyLocationMessage($bot, $event->getReplyToken(), 'LINE', '東京都渋谷区渋谷2-21-1 ヒカリエ27階', 35.659025, 139.703473);
 }
 
 //テキストを送信。引数はLINEBot、返信先、テキスト
@@ -35,6 +37,16 @@ function replyImageMessage($bot, $replyToken, $originalImageUrl,$previewImageUrl
 	if(!$responce->isSucceeded()){
 	error_log('Failed!'. $responce->getHTTPStatus .' '. $responce->getRawBody());
 	}
+}
+
+// 位置情報を返信。引数はLINEBot、返信先、タイトル、住所、
+// 緯度、経度
+function replyLocationMessage($bot, $replyToken, $title, $address, $lat, $lon) {
+  // LocationMessageBuilderの引数はダイアログのタイトル、住所、緯度、経度
+  $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\LocationMessageBuilder($title, $address, $lat, $lon));
+  if (!$response->isSucceeded()) {
+    error_log('Failed!'. $response->getHTTPStatus . ' ' . $response->getRawBody());
+  }
 }
 
 ?>
