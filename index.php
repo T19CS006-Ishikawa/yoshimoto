9ae1bdc5eada4d9f42da2bd0cb73d9686736391e
Shinichiro Watai <g19tk022@yamanashi.ac.jp>
2022-01-15 16:30:29 +0900

    sticker

--- index.php
+++ index.php
@@ -13,8 +13,8 @@ foreach ($events as $event){
 //	$bot->replyText($event->getReplyToken(), 'TextMessage');
 //	replyTextMessage($bot, $event->getReplyToken(),'TextMessage');
 //	replyImageMessage($bot, $event->getReplyToken(),'https://' . $_SERVER['HTTP_HOST'] . '/imgs/original.jpg','https://' . $_SERVER['HTTP_HOST'] . '/imgs/preview.jpg');
-	//位置情報を返信
-	replyLocationMessage($bot, $event->getReplyToken(), 'LINE', '東京都渋谷区渋谷2-21-1 ヒカリエ27階', 35.659025, 139.703473);
+	// スタンプを返信
+	replyStickerMessage($bot, $event->getReplyToken(), 1, 1);
 }
 
 //テキストを送信。引数はLINEBot、返信先、テキスト
@@ -38,13 +38,13 @@ function replyImageMessage($bot, $replyToken, $originalImageUrl,$previewImageUrl
 	}
 }
 
-// 位置情報を返信。引数はLINEBot、返信先、タイトル、住所、
-// 緯度、経度
-function replyLocationMessage($bot, $replyToken, $title, $address, $lat, $lon) {
-  // LocationMessageBuilderの引数はダイアログのタイトル、住所、緯度、経度
-	$response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\LocationMessageBuilder($title, $address, $lat, $lon));
+// スタンプを返信。引数はLINEBot、返信先、
+// スタンプのパッケージID、スタンプID
+function replyStickerMessage($bot, $replyToken, $packageId, $stickerId) {
+	// StickerMessageBuilderの引数はスタンプのパッケージID、スタンプID
+	$response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder($packageId, $stickerId));
 	if (!$response->isSucceeded()) {
-	error_log('Failed!'. $responce->getHTTPStatus .' '. $responce->getRawBody());
+		error_log('Failed!'. $response->getHTTPStatus . ' ' . $response->getRawBody());
 	}
 }
 
