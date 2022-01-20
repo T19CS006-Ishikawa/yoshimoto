21097b4e48fec69374ca7dcea57c835e6fd952f1
Shinichiro Watai <g19tk022@yamanashi.ac.jp>
2022-01-20 16:44:50 +0900

    start

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -52,6 +52,29 @@ class ComposerAutoloaderInit59cd42fa8c407ff3a30639f257ef376b
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit59cd42fa8c407ff3a30639f257ef376b::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire59cd42fa8c407ff3a30639f257ef376b($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequire59cd42fa8c407ff3a30639f257ef376b($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
